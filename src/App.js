56063cc869b73739b3b86362b0e6d8eac89d2513
Vinicius Gabriel Oliveira De Sousa <t-vinicius.sousa@cni.com.br>
2025-01-31 10:39:25 -0300

    :construction:

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { MicVocal} from 'lucide-react'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <header><MicVocal className='mic' />VoiceScriber</header>
+        <div className='hero'>
+          <div className='hero_text'>
+            <h1>Transform Audio into Text with AI Precision</h1>
+            <p>Accurate, fast, and affordable audio transcription powered by advanced AI. Perfect for podcasters, journalists, and businesses.</p>
+          </div>
+        </div>
     </div>
   );
 }
